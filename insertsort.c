643e41673396db004b2573ec0591910c1d520beb
yadongshao <348829719@qq.com>
2015-04-01 08:23:07 +0800

    Create insertsort.c
    插入排序

---
+++ insertsort.c
@@ -0,0 +1,16 @@
+void insertsort(int dat[],int length){
+	int i=0;
+	int j=0;
+	int temp=0;
+	for(i=1;i<length;i++){
+		temp=dat[i];
+		for(j=i;j>0;j--){
+			if(dat[j-1]>temp) dat[j]=dat[j-1];
+			else{
+				break;
+			}
+		}	
+		dat[j]=temp;
+	}
+	display(dat,10);
+}
