870740c6102b945138f1acc93a0d5dbc0a3b69ec
yadongshao <348829719@qq.com>
2015-07-15 10:03:21 +0800

    Create Qsort.cpp

---
+++ Qsort.cpp
@@ -0,0 +1,25 @@
+//快速排序算法  
+int partition_sort(vector<int> &dat,int start,int end){//
+	int std=dat[start];
+	int left=start;
+	int right=end;
+	while (start<end)
+	{
+		while (start<end&&dat[end]>=std)--end;
+		dat[start]=dat[end];
+		while (start<end&&dat[start]<=std)++start;
+		dat[end]=dat[start];
+	}
+	dat[start]=std;//注意：标准就赋值给 上一个赋值给其它位置的元素  因为这个元素没有丢失 
+	return start;
+}
+void qSort(vector<int> &dat,int start,int end){
+	int p;
+	if (start<end)
+	{
+		p=partition_sort(dat,start,end);
+		qSort(dat,start,p-1);
+		qSort(dat,p+1,end);
+	}
+	else return ;
+}
