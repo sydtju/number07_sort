6d222f3358c2341b64a2b044d4fed40494945735
yadongshao <348829719@qq.com>
2015-07-21 10:29:18 +0800

    Update shellsort.c

--- shellsort.c
+++ shellsort.c
@@ -21,3 +21,27 @@ int shellSort(int dat[],int length){
 	}
 	return 0;
 }
+//20150721
+void shellsort(vector<int> &dat){
+	int length=(int)dat.size();
+	if(length==0||length==1) return ;
+	for(int gap=length/2;gap>0;gap=gap/2)
+		for (int i=gap;i<length;++i)
+		{//这里用的是插入排序  插入的方式  是循环链的那种 
+			int temp=dat[i];
+			while (i>=gap&&temp<dat[i-gap])
+			{
+				dat[i]=dat[i-gap];
+				i=i-gap;
+			}
+			dat[i]=temp;
+		}
+}
+void shellsort1(vector<int> &dat){
+	int length=(int)dat.size();
+	if(length==0||length==1) return ;
+	for(int gap=length/2;gap>0;gap=gap/2)
+		for (int i=gap;i<length;++i)
+			for(int j=i;j>=gap&&dat[j]<dat[j-gap];j=j-gap)
+				swap(dat[j],dat[j-gap]);////这里用的是插入排序  插入的方式  是冒泡的那种 其实复杂程度比上面高 但是代码简单 
+}
