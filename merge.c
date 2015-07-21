8d4604b2f8f12ce4624be7471b491b731b28d754
yadongshao <348829719@qq.com>
2015-07-21 12:04:51 +0800

    Update merge.c

--- merge.c
+++ merge.c
@@ -55,3 +55,39 @@ int MergeSort(element *dat,int n){
 	}
 	return 0;
 }
+//20150721 改进版  可以继续改进  从低到高的顺序
+void merge(vector<int> &dat,int start1,int end1,int start2,int end2){
+	vector<int> temp;
+	//int length=(int)dat.size()>end?end:(int)dat.size();
+	end1=end1>(int)dat.size()?dat.size():end1;
+	end2=end2>(int)dat.size()?dat.size():end2;
+	int start=start1>start2?start2:start1;
+	while (start1<end1&&start2<end2)
+	{
+		if (dat[start1]>dat[start2])
+			temp.push_back(dat[start2++]);
+		else
+			temp.push_back(dat[start1++]);
+	}
+	if (start1==end1) 
+		while(start2<end2) 
+			temp.push_back(dat[start2++]);
+	if (start2==end2) 
+		while(start1<end1) 
+			temp.push_back(dat[start1++]);
+	int length=temp.size();
+	for (int i=0;i<length;++i) 
+		dat[i+start]=temp[i];
+}
+void mergesort(vector<int> &dat){
+	int length=1;
+	int maxlength=(int)dat.size();
+	while (length<maxlength)
+	{
+		for (int i=0;i<maxlength;i=i+2*length)
+		{
+			merge(dat,i,i+length,i+length,i+2*length);
+		}
+		length=2*length;
+	}
+} 
